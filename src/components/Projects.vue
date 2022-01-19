ff4551ba98019275e017355fd7a22dea152f9e94
eric4130 <esmrdel413@hotmail.com>
2022-01-19 12:18:45 -0500

    pre TS

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,98 +1,100 @@
 <template>
-<div>
-
+  <div>
     <div class="columns">
-        <div class="column is-6">
-            <a href="https://elizabethsmrdelmassage.com" target="_blank">
-            <div class="card card1">
-                <div class="card-image">
-                    <figure class="image is-4by3">
-                        <img src="../assets/p1.png" alt="Placeholder image">
-                    </figure>
-                </div>
-                <div class="card-content">
-                    <div class="media">
-                        <p class="title is-4">Elizabeth Smrdel, LMT</p>
-                    </div>
-                    <div class="content">
-                        A business-site for a local entrepreneur who specializes in Massage Therapy
-                        <br>
-                    </div>
-                </div>
+      <div class="column is-6">
+        <a href="https://elizabethsmrdelmassage.com" target="_blank">
+          <div class="card card1">
+            <div class="card-image">
+              <figure class="image is-4by3">
+                <img src="../assets/p1.png" alt="Placeholder image" />
+              </figure>
             </div>
-            </a>
-        </div> 
-        <div class="column is-6">
-            <a href='https://votl.netlify.app/' target="_blank">
-            <div class="card card2">
-                <div class="card-image">
-                    <figure class="image is-4by3">
-                        <img src="../assets/p2.png" alt="Placeholder image">
-                    </figure>
-                </div>
-                <div class="card-content">
-                    <div class="media">
-                        <p class="title is-4">Vintage On The Lake</p>
-                    </div>
-                    <div class="content">
-                        An e-commerce project, created with Vue and Django Rest Framework
-                        <br>
-                    </div>
-                </div>
+            <div class="card-content">
+              <div class="media">
+                <p class="title is-4">Elizabeth Smrdel, LMT</p>
+              </div>
+              <div class="content">
+                A business-site for a local entrepreneur who specializes in
+                Massage Therapy
+                <br />
+              </div>
             </div>
-            </a>
-        </div>              
+          </div>
+        </a>
+      </div>
+      <div class="column is-6">
+        <a href="https://votl.netlify.app/" target="_blank">
+          <div class="card card2">
+            <div class="card-image">
+              <figure class="image is-4by3">
+                <img src="../assets/p2.png" alt="Placeholder image" />
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="media">
+                <p class="title is-4">Vintage On The Lake</p>
+              </div>
+              <div class="content">
+                An e-commerce project, created with Vue and Django Rest
+                Framework
+                <br />
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
     </div>
 
     <div class="columns">
-        <div class="column is-6">
-            <a href='https://swapi-todo.netlify.app/' target="_blank">
-            <div class="card card1">
-                <div class="card-image">
-                    <figure class="image is-4by3">
-                        <img src="../assets/p3.png" alt="Placeholder image">
-                    </figure>
-                </div>
-                <div class="card-content">
-                    <div class="media">
-                        <p class="title is-4">SWAPI Todo</p>
-                    </div>
-                    <div class="content">
-                        A site I made to practice connecting to APIs and learn some basic JS and state management
-                        <br>
-                    </div>
-                </div>
+      <div class="column is-6">
+        <a href="https://swapi-todo.netlify.app/" target="_blank">
+          <div class="card card1">
+            <div class="card-image">
+              <figure class="image is-4by3">
+                <img src="../assets/p3.png" alt="Placeholder image" />
+              </figure>
             </div>
-            </a>
-        </div> 
-        <div class="column is-6">
-            <a href='https://eric4130.pythonanywhere.com/' target="_blank">
-            <div class="card card2">
-                <div class="card-image">
-                    <figure class="image is-4by3">
-                        <img src="../assets/p4.png" alt="Placeholder image">
-                    </figure>
-                </div>
-                <div class="card-content">
-                    <div class="media">
-                        <p class="title is-4">Esco Music</p>
-                    </div>
-                    <div class="content">
-                        My project I made completely in Django that is a compendium of information about bands. Make an account and start adding to the database! 
-                        <br>
-                    </div>
-                </div>
+            <div class="card-content">
+              <div class="media">
+                <p class="title is-4">SWAPI Todo</p>
+              </div>
+              <div class="content">
+                A site I made to practice connecting to APIs and learn some
+                basic JS and state management
+                <br />
+              </div>
             </div>
-            </a>
-        </div>              
+          </div>
+        </a>
+      </div>
+      <div class="column is-6">
+        <a href="https://eric4130.pythonanywhere.com/" target="_blank">
+          <div class="card card2">
+            <div class="card-image">
+              <figure class="image is-4by3">
+                <img src="../assets/p4.png" alt="Placeholder image" />
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="media">
+                <p class="title is-4">Esco Music</p>
+              </div>
+              <div class="content">
+                My project I made completely in Django that is a compendium of
+                information about bands. Make an account and start adding to the
+                database!
+                <br />
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
     </div>
-
-</div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Projects'
-}
+  name: "Projects",
+};
 </script>
-
